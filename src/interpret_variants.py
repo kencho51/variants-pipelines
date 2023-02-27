3e5b81e3bd77054f9ab3d5f1ae7b7e124ad1a3fe
Ken <kencho.gigascience@gmail.com>
2023-02-27 03:12:54 +0000

    Make the script testable

--- src/interpret_variants.py
+++ src/interpret_variants.py
@@ -1,35 +1,35 @@
 import pysam
 import pyensembl
 
-# Define paths to input data
-ref_fasta = "/path/to/reference.fasta"
-input_vcf = "/path/to/input.vcf"
+def interpret_variants(ref_fasta, input_vcf):
+    # Load reference genome
+    ref = pysam.FastaFile(ref_fasta)
 
-# Load reference genome
-ref = pysam.FastaFile(ref_fasta)
+    # Load Ensembl genome annotation
+    ensembl = pyensembl.EnsemblRelease(release=98)
 
-# Load Ensembl genome annotation
-ensembl = pyensembl.EnsemblRelease(release=98)
+    # Load input VCF file and interpret variants
+    results = []
+    with open(input_vcf, "r") as f:
+        for line in f:
+            if line.startswith("#"):
+                # Skip VCF header
+                continue
+            fields = line.strip().split("\t")
+            chrom = fields[0]
+            pos = int(fields[1])
+            ref_allele = fields[3]
+            alt_allele = fields[4]
+            # Look up gene name and impact of variant
+            overlapping_genes = ensembl.genes_at_locus(chrom, pos)
+            for gene in overlapping_genes:
+                for transcript in gene.transcripts:
+                    if transcript.overlaps(pos, pos+len(ref_allele)):
+                        effects = transcript.effects(ref_allele, alt_allele, pos)
+                        for effect in effects:
+                            results.append(f"{chrom}:{pos} {ref_allele}>{alt_allele} in {gene.gene_name} ({effect.effect_type})")
 
-# Load input VCF file and interpret variants
-with open(input_vcf, "r") as f:
-    for line in f:
-        if line.startswith("#"):
-            # Skip VCF header
-            continue
-        fields = line.strip().split("\t")
-        chrom = fields[0]
-        pos = int(fields[1])
-        ref_allele = fields[3]
-        alt_allele = fields[4]
-        # Look up gene name and impact of variant
-        overlapping_genes = ensembl.genes_at_locus(chrom, pos)
-        for gene in overlapping_genes:
-            for transcript in gene.transcripts:
-                if transcript.overlaps(pos, pos+len(ref_allele)):
-                    effects = transcript.effects(ref_allele, alt_allele, pos)
-                    for effect in effects:
-                        print(f"{chrom}:{pos} {ref_allele}>{alt_allele} in {gene.gene_name} ({effect.effect_type})")
+    # Clean up resources
+    ref.close()
 
-# Clean up resources
-ref.close()
+    return results
